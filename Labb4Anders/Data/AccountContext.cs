1258a1d0f6efdc6500460cafd4a4d2da3655d2cd
Anders Orlinder <anders_orlinder@hotmail.com>
2020-01-30 21:37:49 +0100

    Adding project files

---
+++ Labb4Anders/Data/AccountContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Labb4Anders
+{
+    public class AccountContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseCosmos("https://localhost:8081",
+                "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+                "Accounts");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
